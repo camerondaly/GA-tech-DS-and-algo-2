0822727823765e45b6abb9e70812dc3e7037862e
Cameron Daly <camerondaly9@gmail.com>
2022-02-13 12:55:27 -0500

    simplified tests

--- Module 6/MinHeapTests.java
+++ Module 6/MinHeapTests.java
@@ -1,18 +1,29 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class MinHeapTests {
 
-    @Test
-    public void testAdd() {
+    public static void main(String[] args) {
+        testAddForResize();
+        testAddForUpHeap();
+    }
+
+    public static void testAddForResize() {
         MinHeap<Integer> testHeap = new MinHeap<>();
-        for (int i = 1; i < 4; i++) {
+        for (int i = 1; i < 13; i++) {
             testHeap.add(i);
         }
-        Integer[] ans = (Integer[]) new Comparable[13];
-        for (int i = 1; i < 4; i++) {
+        Object[] ans = (Object[]) new Comparable[26];
+        for (int i = 1; i < 13; i++) {
             ans[i] = i;
         }
-        assertArrayEquals(ans, testHeap.getBackingArray());
+        System.out.println(ans.equals(testHeap.getBackingArray()));
+    }
+
+    public static void testAddForUpHeap() {
+        MinHeap<Integer> testHeap = new MinHeap<>();
+        for (int i = 1; i < 9; i++) {
+            testHeap.add(i);
+        }
+        Integer[] ans = new Integer[]{null, 0, 1, 3, 4, 2, 6, 7, 8, 9, 5, null ,null};
+        testHeap.add(0);
+        System.out.println(ans.equals(testHeap.getBackingArray()));
     }
 }
